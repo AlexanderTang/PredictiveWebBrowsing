791dc8e9648cfbf272af3f65df9ccdbdc45445a9
Daniel Andrés Pérez Pérez <dapp.1990@gmail.com>
2016-04-29 11:39:24 +0200

    get_session method added

--- program/preprocessing.py
+++ program/preprocessing.py
@@ -1,6 +1,8 @@
 import load
 import operator
 
+threshold = 1000
+
 def count_occurrences():
     dataset = load.load()
     dom_dict = {}
@@ -20,4 +22,34 @@ def sort_occurrences():
     for el in sorted_x:
         print el
 
+def get_sessions():
+    # In this case I was loading the rows with load actions of the transformed_data file, now load is loading the
+    # transformed_data as it is.
+    dataset = load.load()
+
+    l = len(dataset)
+
+    for i in range(0,l-1):
+        rowTime1 = dataset[i][1]
+        rowTime2 = dataset[i+1][1]
+
+        # The following two lines are use to remove the extra colon at the beginning of the some times
+        if rowTime1[0] == ":":
+            rowTime1 = rowTime1[1:]
+
+        if rowTime2[0] == ":":
+            rowTime2 = rowTime2[1:]
+
+        deltaTime = np.datetime64(dataset[i+1][0]+"T"+rowTime2) - np.datetime64(dataset[i][0]+"T"+rowTime1)
+
+        if deltaTime > threshold:
+            #dataset[i+1] should be start the new session
+        else
+            # dataset[i] remains in the session
+
 sort_occurrences()
+
+
+"""
+Prolling referes to call to external source, not made by user, so probably it is not necessary
+"""
---
+++ program/test.py
@@ -0,0 +1,35 @@
+import load
+import numpy as np
+
+def test_method():
+    dataset = load.load()
+    test = []
+    dom_dict = {}
+    for row in dataset:
+        if row[2] == "load":
+            test.append(row)
+    return test
+
+thresholder = 1000
+
+def count_occurrences():
+    dataset = test_method()
+    l = len(dataset)
+
+    for i in range(0,l-1):
+        rowTime1 = dataset[i][1]
+        rowTime2 = dataset[i+1][1]
+
+        if rowTime1[0] == ":":
+            rowTime1 = rowTime1[1:]
+
+        if rowTime2[0] == ":":
+            rowTime2 = rowTime2[1:]
+
+        deltaTime = np.datetime64(dataset[i+1][0]+"T"+rowTime2) - np.datetime64(dataset[i][0]+"T"+rowTime1)
+
+        if deltaTime > thresholder:
+            print dataset[i]
+
+count_occurrences()
+#print test_method()
