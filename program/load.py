46476f296466257a7e762448593bbdf562d5958a
Babyburger <alexandertang357@hotmail.com>
2016-04-29 11:39:06 +0200

    create a ad filter container

--- program/load.py
+++ program/load.py
@@ -3,10 +3,12 @@ import numpy as np
 
 # loads the transformed data set and returns it as a numpy array
 def load():
-    return np.genfromtxt('../processed_data/transformed_data.csv', delimiter=",",dtype=str)
+    return np.genfromtxt('../processed_data/transformed_data.csv', delimiter=",", dtype=str)
 
 
 # load the transformed data set without 'beforeunload' and 'polling' as a numpy array
+# loaded urls which are considered spam ads are also filtered
+#   (urls are ads when they load the same page within 10 milliseconds of each other)
 def downsized_load():
     dataset = load()
     dataset = dataset[
@@ -15,4 +17,14 @@ def downsized_load():
             dataset[:, 2] == "polling"
         ))
     ]
+    dataset = filter_ads(dataset)
     return dataset
+
+
+def filter_ads(dataset):
+
+
+
+a = downsized_load()
+for el in a:
+    print el
