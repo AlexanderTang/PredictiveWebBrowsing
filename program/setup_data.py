077c61945e228fe1951d478bc9a4e6bdffae462c
Babyburger <alexandertang357@hotmail.com>
2016-05-03 15:43:59 +0200

    file writes implemented; logic still has to be done

--- program/setup_data.py
+++ program/setup_data.py
@@ -1,5 +1,11 @@
 import load
 import csv
+import numpy as np
+
+# threshold for users to become eligible for the ground truth
+#   (see define_truth_users() method)
+USER_ROWS_THRESHOLD = 30
+
 
 # generates the ground truth for all users and store it in gt_all.csv
 def define_truth_all():
@@ -13,11 +19,26 @@ def define_truth_all():
 
 
 # generates the ground truth for each user and store it
-# in separate .csv files
+# in separate .csv files; users with less than 30 rows are
+# not processed due to too small sample size
 def define_truth_users():
     dataset = load.deep_filtered_load()
+    # split the dataset into submatrices by user ID
+    split_dataset = np.split(dataset,
+            np.where( np.diff(dataset["uid"]) )[0] + 1
+        )
 
-    # IMPLEMENT: loop over all users and call gen truth; save to different csv file
+    uid = 1
+    for data in split_dataset:
+        dataset = gen_truth(data)
+        if len(dataset["uid"]) >= USER_ROWS_THRESHOLD:
+            path = "../ground_truth/gt_u" + str(uid) + ".csv"
+
+            with open(path, 'wb') as csvfile:
+                csvwriter = csv.writer(csvfile, delimiter=',')
+                for row in dataset:
+                    csvwriter.writerow(row)
+        uid += 1
 
 
 # generates and returns the ground truth
@@ -25,3 +46,6 @@ def gen_truth(dataset):
     # TO IMPLEMENT
 
     return dataset
+
+define_truth_users()
+
