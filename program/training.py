254858120aad38614598a5d6421d21eb98662ade
Babyburger <alexandertang357@hotmail.com>
2016-05-05 23:55:48 +0200

    training datasets completed for all percentages

--- program/training.py
+++ program/training.py
@@ -12,14 +12,14 @@ def set_training_testing_data():
         file_name = csv_file.rsplit('\\', 1)[1]  # get filename with extension .csv
         file_name = file_name[3:]
 
-        write_training_data(0.5, "50_50", file_name, csv_matrix)
-        write_training_data(0.6, "60_40", file_name, csv_matrix)
-        write_training_data(0.7, "70_30", file_name, csv_matrix)
-        write_training_data(0.8, "80_20", file_name, csv_matrix)
+        write_data(0.5, "50_50", file_name, csv_matrix)
+        write_data(0.6, "60_40", file_name, csv_matrix)
+        write_data(0.7, "70_30", file_name, csv_matrix)
+        write_data(0.8, "80_20", file_name, csv_matrix)
 
 
-def write_training_data(training_percentage, sub_folder, file_name, csv_matrix):
-    training_data, testing_data = train_test_split(csv_matrix, training_size=training_percentage)
+def write_data(training_percentage, sub_folder, file_name, csv_matrix):
+    training_data, testing_data = train_test_split(csv_matrix, train_size=training_percentage)
 
     training_path = "../training_data/" + sub_folder + "/" + file_name
     testing_path = "../testing_data/" + sub_folder + "/" + file_name
@@ -34,4 +34,5 @@ def write_training_data(training_percentage, sub_folder, file_name, csv_matrix):
         for row in testing_data:
             csvwriter.writerow(row)
 
-    return training_data
+
+#set_training_testing_data()
