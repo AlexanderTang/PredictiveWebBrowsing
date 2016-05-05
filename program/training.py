d1bd3d17b834e5ecbbd506115609debb1bfbddfe
Babyburger <alexandertang357@hotmail.com>
2016-05-05 23:39:57 +0200

    storing training work

--- program/training.py
+++ program/training.py
@@ -4,23 +4,27 @@ import numpy as np
 import csv
 
 
+def set_training_testing_data():
+    csv_list = glob.glob('../ground_truth/*.csv')
+    for csv_file in csv_list:
+        csv_matrix = np.genfromtxt(csv_file, delimiter=",", dtype=None,
+                                   names=["key", "result"])
+        file_name = csv_file.rsplit('\\', 1)[1]  # get filename with extension .csv
+        file_name = file_name[3:]
+
+        write_training_data(0.5, "50_50", file_name, csv_matrix)
+        write_training_data(0.6, "60_40", file_name, csv_matrix)
+        write_training_data(0.7, "70_30", file_name, csv_matrix)
+        write_training_data(0.8, "80_20", file_name, csv_matrix)
 
 
+def write_training_data(training_percentage, sub_folder, file_name, csv_matrix):
+    training_data, testing_data = train_test_split(csv_matrix, training_size=training_percentage)
 
-    if user_id == 0:
-        raw_data = np.genfromtxt('../ground_truth/gt_all.csv', delimiter=",", dtype=None,
-                                names=["ts", "action", "dom", "path", "uid"])
-    else:
-        file_path = "../ground_truth/gt_u" + str(user_id) + ".csv"
-        raw_data = np.genfromtxt(file_path, delimiter=",", dtype=None,
-                                names=["ts", "action", "dom", "path", "uid"])
+    training_path = "../training_data/" + sub_folder + "/" + file_name
+    testing_path = "../testing_data/" + sub_folder + "/" + file_name
 
-    training_data, testing_data = train_test_split(raw_data, test_size=TRAINING_DATA_PERCENTAGE)
-
-    traning_path = "../training_data/training_" + str(user_id) + ".csv"
-    testing_path = "../testing_data/testing_" + str(user_id) + ".csv"
-
-    with open(traning_path, 'wb') as csvfile:
+    with open(training_path, 'wb') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         for row in training_data:
             csvwriter.writerow(row)
